28520419b64d6bf62723bbee042c7896efff0e00
vrbprog <vrbdsp@gmail.com>
2024-12-25 11:09:30 +0200

    Init repo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,7 @@
 
-import User from './profile/Profile';
-import FriendList from './friendlist/FriendList';
-import Transactions from './transactionhistory/TransactionHistory'
-import userData from "../../userData.json";
-import friendsList from "../../friends.json";
-import transactionsList from "../../transactions.json";
-
-
 export default function App() {
   return (
     <main>
-      <User
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats} />
-      
-      <FriendList
-        friends={friendsList}
-      />
-
-      <Transactions
-        transactions={transactionsList}
-      />
 
     </main>
   );
